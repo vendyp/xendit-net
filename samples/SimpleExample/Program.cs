098b57c1b19f3ff1ca9f9efee00452f595316e97
Dwi Vendy Pratama <dwivendypratama@gmail.com>
2021-09-26 02:28:50 +0700

    Add sample project

---
+++ samples/SimpleExample/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Xendit.Net;
+
+namespace SimpleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cfg = new XenditOptions()
+            {
+                //REPLACE THIS KEY WITH YOURS
+                ApiKey = "xnd_development_O46JfOtygef9kMNsK+ZPGT+ZZ9b3ooF4w3Dn+R1k+2fT/7GlCAN3jg=="
+            };
+
+            var client = new XenditClient(cfg);
+
+            var balance = new BalanceService(client);
+
+            var balanceResult = balance.GetAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine($"Your current balance : {balanceResult.Balance}");
+            Console.ReadKey();
+        }
+    }
+}
